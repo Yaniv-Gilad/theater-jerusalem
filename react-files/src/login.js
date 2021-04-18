3da5c487fe022afc84b9763d89f8fd0b4456b5c9
Yael <yaeliberman123@gmail.com>
2021-04-18 09:32:07 +0300

    login page

---
+++ react-files/src/login.js
@@ -0,0 +1,67 @@
+
+import {Component} from "react"
+import firebase from "firebase"
+import {auth} from "./Firebase/firebase"
+import './App.css'
+
+class Login extends Component
+{
+    constructor(props) {
+        super(props);
+        this.state ={
+          email:"",
+          password:"",
+          number:0
+        }
+    }
+
+   render() {
+    return( 
+        <div> 
+           <h1>Login</h1>
+           <input type="email"
+           id="email"
+           placeholder="userName@email.com"
+           title="email"
+           //required = {true}
+           onChange={(event)=>{
+             this.setState({email:event.target.value})
+             //console.log((event.target.value))
+           }}
+           />
+
+          <br/>
+          <input type="password"
+           id="password"
+           placeholder="password"
+           title="password"
+           //required = {true}
+           onChange={(event)=>{
+             this.setState({password:event.target.value})
+             //console.log((event.target.value))
+           }}
+           />
+
+            <br/>
+             <button onClick={()=>{
+               this.login()
+            console.log(this.state)
+               }}>Login</button>
+           </div>
+           
+      )
+   }
+
+   login()
+   {
+     auth.signInWithEmailAndPassword(this.state.email, this.state.password)
+     .then((res)=>{
+       console.log("can login")
+        console.log(res)
+     }).catch((e)=>{
+       console.log("not found user")
+     })
+   }
+  }
+
+export default Login;
