edc153f2f5ccd80d2ac8e6c2b7647169078463b6
yaniv <yanivgilad96@gmail.com>
2021-04-25 10:45:57 +0300

    check if user logged in

--- react-files/src/Components/login.js
+++ react-files/src/Components/login.js
@@ -1,72 +1,70 @@
-import {Component} from "react"
-import {auth} from "../Firebase/firebase"
+import { Component } from "react"
+import { auth } from "../Firebase/firebase"
 import '../App.css'
 
 
-class Login extends Component
-{
-    constructor(props) {
-        super(props);
-        this.state ={
-          email:"",
-          password:""
-        }
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
     }
+  }
 
 
-   render() {
-    return( 
-        <div> 
-           <h1>Login</h1>
-           <input type="email"
-           id="email"
-           placeholder="userName@email.com"
-           title="email"
-           //required = {true}
-           onChange={(event)=>{
-             this.setState({email:event.target.value})
-             //console.log((event.target.value))
-           }}
-           />
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <input type="email"
+          id="email"
+          placeholder="userName@email.com"
+          title="email"
+          //required = {true}
+          onChange={(event) => {
+            this.setState({ email: event.target.value })
+            //console.log((event.target.value))
+          }}
+        />
 
-          <br/>
-          <input type="password"
-           id="password"
-           placeholder="password"
-           title="password"
-           //required = {true}
-           onChange={(event)=>{
-             this.setState({password:event.target.value})
-             //console.log((event.target.value))
-           }}
-           />
+        <br />
+        <input type="password"
+          id="password"
+          placeholder="password"
+          title="password"
+          //required = {true}
+          onChange={(event) => {
+            this.setState({ password: event.target.value })
+            //console.log((event.target.value))
+          }}
+        />
 
-            <br/>
-             <button onClick={()=>{
-               this.login()
-            console.log(this.state)
-               }}>Login</button>
-           </div>
-           
-      )
-   }
+        <br />
+        <button onClick={() => {
+          this.login()
+          console.log(this.state)
+        }}>Login</button>
+      </div>
 
-   login()
-   {
-     auth.signInWithEmailAndPassword(this.state.email, this.state.password)
-     .then((res)=>{
-       console.log("can login")
+    )
+  }
+
+  login() {
+    auth.signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((res) => {
+        console.log("can login")
         console.log(res)
         this.props.history.push(
           {
             pathname: "/newPage",
-            data:res.user
+            data: res.user
           })
-        
-     }).catch((e)=>{
-       console.log("not found user")
-     })
-   }
+
+      }).catch((e) => {
+        console.log("not found user")
+      })
   }
+}
 
 export default Login;
--- react-files/src/Firebase/firebase.js
+++ react-files/src/Firebase/firebase.js
@@ -4,19 +4,17 @@ import 'firebase/firebase-firestore'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAksCt4qgYCa30WSxy-tX8yq39dX9Lj0RI",
-    authDomain: "theater-841bd.firebaseapp.com",
-    databaseURL: "https://theater-841bd-default-rtdb.firebaseio.com",
-    projectId: "theater-841bd",
-    storageBucket: "theater-841bd.appspot.com",
-    messagingSenderId: "796389253256",
-    appId: "1:796389253256:web:32b6d13276c27b49b1ff3d",
-    measurementId: "G-J14DSFVW3J"
-  };
+  apiKey: "AIzaSyAksCt4qgYCa30WSxy-tX8yq39dX9Lj0RI",
+  authDomain: "theater-841bd.firebaseapp.com",
+  databaseURL: "https://theater-841bd-default-rtdb.firebaseio.com",
+  projectId: "theater-841bd",
+  storageBucket: "theater-841bd.appspot.com",
+  messagingSenderId: "796389253256",
+  appId: "1:796389253256:web:32b6d13276c27b49b1ff3d",
+  measurementId: "G-J14DSFVW3J"
+};
 
-  // Initialize Firebase
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const db = firebase.firestore();
-
-  
