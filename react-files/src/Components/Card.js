509660d7fce8f89ce7a988eee19d02a22185f121
Yael <yaeliberman123@gmail.com>
2021-04-29 21:49:25 +0300

    Update Card.js

--- react-files/src/Components/Card.js
+++ react-files/src/Components/Card.js
@@ -20,24 +20,27 @@ import LOGO from "../Photos/logo.jpeg"
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
+    display: "inline-block",
+    // paddingInlineStart: '2%',
+    // margin: '2%'
   },
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
   },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
+//   expand: {
+//     transform: 'rotate(0deg)',
+//     marginLeft: 'auto',
+//     transition: theme.transitions.create('transform', {
+//       duration: theme.transitions.duration.shortest,
+//     }),
+//   },
+//   expandOpen: {
+//     transform: 'rotate(180deg)',
+//   },
+//   avatar: {
+//     backgroundColor: red[500],
+//   },
 }));
 
 export default function RecipeReviewCard(result) {
