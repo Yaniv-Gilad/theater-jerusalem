1f2734035f3775c86d1119810ee081e8c43adb24
Yael <yaeliberman123@gmail.com>
2021-04-29 20:58:09 +0300

    Create File_comp.js

---
+++ react-files/src/Components/File_comp.js
@@ -0,0 +1,34 @@
+import { Component } from "react"
+import { auth } from "../Firebase/firebase"
+import '../CSS/Login.css'
+import LOGO from "../Photos/logo.jpeg"
+import Card from "./Card.js"
+import Data from "../users.json"
+
+
+class File_comp extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+  render() {
+    return (
+      <div className="File_comp">
+        {Data.map((data,index) =>(
+        <div>
+            <Card name={Data.name}/>
+            {console.log(Data.name)}
+        </div>
+        ))}
+          
+      </div>
+
+    )
+  }
+
+
+  
+}
+
+export default File_comp;
