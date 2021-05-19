d927b8aaa9434e91811144e2ef64e4f490d45c34
Yael <yaeliberman123@gmail.com>
2021-05-19 13:11:24 +0300

    Update App.js

--- react-files/src/App.js
+++ react-files/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Footer from "./Components/Footer";
 import Login from "./Components/Login";
 import HomePage from "./Components/HomePage";
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
